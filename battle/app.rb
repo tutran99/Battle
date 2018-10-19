5c8335fb335100bf069d563e4a5038902ab16e17
Makers Academy <onboarding@makersacademy.com>
2018-10-19 10:08:17 +0000

    Add player.rb for Model Layer

--- battle/app.rb
+++ battle/app.rb
@@ -8,20 +8,20 @@ get '/' do
 end
   
 post '/names' do
-  session[:player_1] = params[:player_1]
-  session[:player_2] = params[:player_2]
+  $player_1 = Player.new(params[:player_1])
+  $player_2 = Player.new(params[:player_2])
   redirect '/play'
 end
 
 get '/play' do
-  @player_1 = session[:player_1]
-  @player_2 = session[:player_2]
+  @player_1 = $player_1.name
+  @player_2 = $player_2.name
   erb :play
 end
 
 get '/attack' do
-  @player_1 = session[:player_1]
-  @player_2 = session[:player_2]
+  @player_1 = $player_1.name
+  @player_2 = $player_2.name
   erb :attack
  end
     run! if app_file == $0
